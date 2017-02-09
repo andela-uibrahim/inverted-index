e4a581671ee85499c93d6b7a42dd9db3b3d1cccb
usman ibrahim <usman.ibrahim@andela.com>
2017-02-09 15:10:11 -0500

    chore (Gulp): Implementation of automated browser refresh using gulp and browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+gulp.task('browser', ()=>{
+	browserSync.init({
+		server : {
+			baseDir : 'public'
+		}
+	});
+});
+
+gulp.task('watch',['browserSync'], ()=>{
+	gulp.watch('public/index.html',browserSync.reload);
+	gulp.watch('public/css/*.css',browserSync.reload);
+	gulp.watch('public/js/*.js',browserSync.reload);
+})
