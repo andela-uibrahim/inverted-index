f3c4b6a99c4eedf4fed0da0007f8324243b2bf70
usman ibrahim <usman.ibrahim@andela.com>
2017-02-12 15:51:43 -0500

    feature(UI): layed out a simple user interface for the app

---
+++ public/js/index.js
@@ -0,0 +1,31 @@
+const myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(($routeProvider) => {
+  $routeProvider
+   // home pages
+  .when('/', {
+    templateUrl: 'templates/home.html',
+    controller: 'homeController'
+  })
+
+   // index pages
+  .when('/showIndex', {
+    templateUrl: 'templates/table.html',
+    controller: 'tableController'
+  })
+
+// search pages
+  .when('/searchIndex', {
+    templateUrl: 'templates/search.html',
+    controller: 'searchController'
+  });
+});
+
+
+myApp.controller('homeController',
+  ['$scope', '$location', ($scope, $location) => {
+    $scope.submit = () => {
+      $location.path('/showIndex');
+    };
+  }]);
+
--- public/js/inverted_index.js
+++ public/js/inverted_index.js
@@ -1,1 +1,8 @@
 const invertedIndex = () => true;
+// $('#btn').click(function() {
+			// 	$('.side-menu').fadeToggle();
+			// });
+
+$('.upload').click(() => {
+  $('#upload').trigger('click');
+});
