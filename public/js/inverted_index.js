d7f9ecbba3d8b8842af9503ff1af900d58aeca79
usman ibrahim <usman.ibrahim@andela.com>
2017-02-18 13:00:31 -0500

    setting up unit tests

--- public/js/inverted_index.js
+++ public/js/inverted_index.js
@@ -1,1 +1,19 @@
-const invertedIndex = () => true;
+class InvertedIndex {
+  creatIndex(book) {
+    this.book = book;
+    this.index = {};
+    for (let i = 0; i < book.length; i++) {
+      let words = (book[i].title + ' ' + book[i].text).split(' ');
+      for (let j = 0; j < words.length; j++) {
+        if (this.index[words[j]]) {
+          this.index[words[j]].push(i);
+        } else {
+          this.index[words[j]] = [];
+          this.index[words[j]].push(i);
+        }
+      }
+    }
+
+    console.log(this.index);
+  }
+}
--- spec/upload_spec.js
+++ spec/upload_spec.js
@@ -1,5 +1,19 @@
-describe('A suite', () => {
-  it('contains spec with an expectation', () => {
-    expect(invertedIndex()).toBe(true);
+const createIndex = new InvertedIndex();
+
+describe('Test if the object exist and new instances can be created from it', () => {
+  beforeEach(() => {
   });
+
+  it('contains spec with an expectation', () => {
+    expect(typeof (InvertedIndex)).toEqual('function');
+  });
+
+  it('should create an object once the class is declared', () => {
+    expect(createIndex).toEqual(jasmine.any(Object));
+  });
+
+  it('should create an instance of itself once its attached to the "new" key word',
+   () => {
+     expect(createIndex instanceof InvertedIndex).toBeTruthy();
+   });
 });
