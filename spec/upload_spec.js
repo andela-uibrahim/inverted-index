fddc603b66d50b0324b14e0fe3f177c8f8a65376
usman ibrahim <usman.ibrahim@andela.com>
2017-02-22 02:58:12 -0500

    feature(createIndex): added the create index functionality

--- spec/upload_spec.js
+++ spec/upload_spec.js
@@ -1,5 +1,19 @@
-describe('Test for OOP techniques', () => {
-  it('expect "type of invertedIndex" to be a "function"', () => {
-    expect(typeof (InvertedIndex).toEqual('function'));
+const invertedIndex = new InvertedIndex();
+describe('Test if the object exist and new instances can be created from it', () => {
+  beforeEach(() => {
   });
+
+  it('contains spec with an expectation', () => {
+    expect(typeof (InvertedIndex)).toEqual('function');
+  });
+
+  it('should create an object once the class is declared', () => {
+    expect(invertedIndex).toEqual(jasmine.any(Object));
+  });
+
+  it('should create an instance of itself once its attached to the' +
+   '"new" key word',
+   () => {
+     expect(invertedIndex instanceof InvertedIndex).toBeTruthy();
+   });
 });
