bb43f80721739a1ba82399cab930bec4c33d8c2e
usman ibrahim <usman.ibrahim@andela.com>
2017-03-03 07:29:37 -0500

    chore(refactor): fixed file routing problem #7

--- public/js/inverted_indexUI/directives.js
+++ public/js/inverted_indexUI/directives.js
@@ -1,10 +1,3 @@
-// myApp.directive('indexTab', () => ({
-//   templateUrl: 'templates/tabContent.html',
-//   replace: 'true',
-//   scope: {
-//     indexedFiles: '='
-//   },
-// }));
 
 myApp.directive('searchResult', () => ({
   templateUrl: 'templates/searchContent.html',
